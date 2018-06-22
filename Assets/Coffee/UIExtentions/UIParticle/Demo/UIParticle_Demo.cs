3a40062e2c106cf81569a29830115d4f8c8ac4a8
mob-sakai <sakai@mobcast.jp>
2018-06-22 18:09:06 +0900

    Add demo

---
+++ Assets/Coffee/UIExtentions/UIParticle/Demo/UIParticle_Demo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Coffee.UIExtensions.Demo
+{
+	public class UIParticle_Demo : MonoBehaviour
+	{
+		public void SetTimeScale(float scale)
+		{
+			Time.timeScale = scale;
+		}
+	}
+}
