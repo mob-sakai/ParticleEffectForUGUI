b6f23013f8f2576e619a70f36bb665ba2580c43f
mob-sakai <sakai@mobcast.jp>
2018-07-13 14:47:30 +0900

    Add try-catch

--- UIParticle.cs
+++ UIParticle.cs
@@ -105,72 +105,79 @@ namespace Coffee.UIExtensions
 
 		void UpdateMesh()
 		{
-			Profiler.BeginSample("CheckTrail");
-			CheckTrail();
-			Profiler.EndSample();
-
-			if (m_ParticleSystem)
+			try
 			{
-				Profiler.BeginSample("Disable ParticleSystemRenderer");
-				if (Application.isPlaying)
-				{
-					_renderer.enabled = false;
-				}
+				Profiler.BeginSample("CheckTrail");
+				CheckTrail();
 				Profiler.EndSample();
 
-				Profiler.BeginSample("Make Matrix");
-				var cam = canvas.worldCamera ?? Camera.main;
-				bool useTransform = false;
-				Matrix4x4 matrix = default(Matrix4x4);
-				switch (m_ParticleSystem.main.simulationSpace)
+				if (m_ParticleSystem)
 				{
-					case ParticleSystemSimulationSpace.Local:
-						matrix =
-						Matrix4x4.Rotate(m_ParticleSystem.transform.rotation).inverse
-						 * Matrix4x4.Scale(m_ParticleSystem.transform.lossyScale).inverse;
-						useTransform = true;
-						break;
-					case ParticleSystemSimulationSpace.World:
-						matrix = m_ParticleSystem.transform.worldToLocalMatrix;
-						break;
-					case ParticleSystemSimulationSpace.Custom:
-						break;
-				}
-				Profiler.EndSample();
-
-				_mesh.Clear();
-				if (0 < m_ParticleSystem.particleCount)
-				{
-					Profiler.BeginSample("Bake Mesh");
-					if (m_IsTrail)
+					Profiler.BeginSample("Disable ParticleSystemRenderer");
+					if (Application.isPlaying)
 					{
-						_renderer.BakeTrailsMesh(_mesh, cam, useTransform);
-					}
-					else
-					{
-						_renderer.BakeMesh(_mesh, cam, useTransform);
+						_renderer.enabled = false;
 					}
 					Profiler.EndSample();
 
-					// Apply matrix.
-					Profiler.BeginSample("Apply matrix to position");
-					_mesh.GetVertices(s_Vertices);
-					var count = s_Vertices.Count;
-					for (int i = 0; i < count; i++)
+					Profiler.BeginSample("Make Matrix");
+					var cam = canvas.worldCamera ?? Camera.main;
+					bool useTransform = false;
+					Matrix4x4 matrix = default(Matrix4x4);
+					switch (m_ParticleSystem.main.simulationSpace)
 					{
-						s_Vertices[i] = matrix.MultiplyPoint3x4(s_Vertices[i]);
+						case ParticleSystemSimulationSpace.Local:
+							matrix =
+							Matrix4x4.Rotate(m_ParticleSystem.transform.rotation).inverse
+							 * Matrix4x4.Scale(m_ParticleSystem.transform.lossyScale).inverse;
+							useTransform = true;
+							break;
+						case ParticleSystemSimulationSpace.World:
+							matrix = m_ParticleSystem.transform.worldToLocalMatrix;
+							break;
+						case ParticleSystemSimulationSpace.Custom:
+							break;
 					}
-					_mesh.SetVertices(s_Vertices);
-					s_Vertices.Clear();
+					Profiler.EndSample();
+
+					_mesh.Clear();
+					if (0 < m_ParticleSystem.particleCount)
+					{
+						Profiler.BeginSample("Bake Mesh");
+						if (m_IsTrail)
+						{
+							_renderer.BakeTrailsMesh(_mesh, cam, useTransform);
+						}
+						else
+						{
+							_renderer.BakeMesh(_mesh, cam, useTransform);
+						}
+						Profiler.EndSample();
+
+						// Apply matrix.
+						Profiler.BeginSample("Apply matrix to position");
+						_mesh.GetVertices(s_Vertices);
+						var count = s_Vertices.Count;
+						for (int i = 0; i < count; i++)
+						{
+							s_Vertices[i] = matrix.MultiplyPoint3x4(s_Vertices[i]);
+						}
+						_mesh.SetVertices(s_Vertices);
+						s_Vertices.Clear();
+						Profiler.EndSample();
+					}
+
+
+					// Set mesh to CanvasRenderer.
+					Profiler.BeginSample("Set mesh and texture to CanvasRenderer");
+					canvasRenderer.SetMesh(_mesh);
+					canvasRenderer.SetTexture(mainTexture);
 					Profiler.EndSample();
 				}
-
-
-				// Set mesh to CanvasRenderer.
-				Profiler.BeginSample("Set mesh and texture to CanvasRenderer");
-				canvasRenderer.SetMesh(_mesh);
-				canvasRenderer.SetTexture(mainTexture);
-				Profiler.EndSample();
+			}
+			catch(System.Exception e)
+			{
+				Debug.LogException(e);
 			}
 		}
 
