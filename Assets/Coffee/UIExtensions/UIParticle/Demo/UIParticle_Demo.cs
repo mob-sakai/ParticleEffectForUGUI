5e6cd8c8f693d7b65339856a7f5ccf941dd75f38
mob-sakai <sakai861104@gmail.com>
2018-11-28 14:19:48 +0900

    Update demo

--- Assets/Coffee/UIExtensions/UIParticle/Demo/UIParticle_Demo.cs
+++ Assets/Coffee/UIExtensions/UIParticle/Demo/UIParticle_Demo.cs
@@ -6,38 +6,46 @@ namespace Coffee.UIExtensions.Demo
 	public class UIParticle_Demo : MonoBehaviour
 	{
 		[SerializeField] Sprite m_Sprite;
-		[SerializeField] ParticleSystem[] m_ParticleSystems;
-		[SerializeField] Mask[] m_Masks;
-	
-		public void SetTimeScale(float scale)
+		[SerializeField] ParticleSystem [] m_ParticleSystems;
+		[SerializeField] Mask [] m_Masks;
+		[SerializeField] Transform m_ScalingByTransform;
+		[SerializeField] UIParticle m_ScalingByUIParticle;
+
+		public void SetTimeScale (float scale)
 		{
 			Time.timeScale = scale;
 		}
-		
-		public void EnableTrailRibbon(bool ribbonMode)
+
+		public void EnableTrailRibbon (bool ribbonMode)
 		{
-			foreach(var p in m_ParticleSystems)
+			foreach (var p in m_ParticleSystems)
 			{
 				var trails = p.trails;
 				trails.mode = ribbonMode ? ParticleSystemTrailMode.Ribbon : ParticleSystemTrailMode.PerParticle;
 			}
 		}
-		
-		public void EnableSprite(bool enabled)
+
+		public void EnableSprite (bool enabled)
 		{
-			foreach(var p in m_ParticleSystems)
+			foreach (var p in m_ParticleSystems)
 			{
 				var tex = p.textureSheetAnimation;
 				tex.enabled = enabled;
 			}
 		}
-		
-		public void EnableMask(bool enabled)
+
+		public void EnableMask (bool enabled)
 		{
-			foreach(var m in m_Masks)
+			foreach (var m in m_Masks)
 			{
 				m.enabled = enabled;
 			}
+		}
+
+		public void SetScale (float scale)
+		{
+			m_ScalingByTransform.localScale = Vector3.one * (10 * scale);
+			m_ScalingByUIParticle.scale = scale;
 		}
 	}
 }
